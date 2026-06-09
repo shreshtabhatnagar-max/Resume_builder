ecfff02ea5331a69aafb91c8ee6c4a61c9c8f783
Shresth Bhatnagar <shreshtabhatnagar@gmail.com>
2026-06-09 20:36:01 +0530

    Fix Spring Security CORS

--- src/main/java/com/example/Resume_Builder/config/SecurityConfig.java
+++ src/main/java/com/example/Resume_Builder/config/SecurityConfig.java
@@ -49,7 +49,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOriginPatterns(Arrays.asList(
+                "http://localhost:*",
+                "https://*.vercel.app"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
